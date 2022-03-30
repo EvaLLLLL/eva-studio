95c2de73e589f74b642541e2b1c1c5117bda8e11
lyhhh <17610603221@163.com>
2022-03-30 12:32:59 +0800

    custom document

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     // ssr and displayName are configured by default
+
+    // Currently, only the ssr and displayName transforms have been implemented. These two transforms are the main requirement for using styled-components in Next.js.
     styledComponents: true,
   },
 
