59dcbcd67c9f1400f841af1718726c7b10ac7523
lyhhh <17610603221@163.com>
2022-03-30 13:05:25 +0800

    add babel config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // ssr and displayName are configured by default
-
-    // Currently, only the ssr and displayName transforms have been implemented. These two transforms are the main requirement for using styled-components in Next.js.
-    styledComponents: true,
-  },
 
   webpack(config) {
     config.module.rules.push({
